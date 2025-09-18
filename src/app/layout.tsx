c7783dc57419527932be10bd20edd5c1d0877ede
Joyce Kwak <joycekwak73@gmail.com>
2025-09-18 14:40:53 -0400

    fixed favicon issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -139,10 +139,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/profile-pic.png" sizes="any" />
-        <link rel="apple-touch-icon" href="/profile-pic.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#6EE7B7" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/profile-pic.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/profile-pic.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="/profile-pic.png" />
+<link rel="manifest" href="/manifest.json" />
+<meta name="theme-color" content="#6EE7B7" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${caveat.variable} ${instrumentSerif.variable} ${carlito.variable} ${philosopher.variable} ${mulish.variable} ${lavishlyYours.variable} ${inter.variable} ${orbit.variable} ${gildaDisplay.variable} antialiased`}
